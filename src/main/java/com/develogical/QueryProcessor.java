83a653f72160abdeb0635d217992bb768b6bb23b
Isaac Song <isong2@isaacs-mbp-3.wifi.local.cmu.edu>
2022-09-22 11:01:32 -0400

    Largest integer implementation done

--- src/main/java/com/develogical/QueryProcessor.java
+++ src/main/java/com/develogical/QueryProcessor.java
@@ -1,5 +1,9 @@
 package com.develogical;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class QueryProcessor {
 
     public String process(String query) {
@@ -16,6 +20,30 @@ public class QueryProcessor {
         if (query.toLowerCase().contains("name")) {
             return "curry";
         }
+
+        if (query.toLowerCase().contains("largest")) {
+            String[] splitquery = query.split("%");
+            List<Integer> numList = new ArrayList<>();
+            for (int i = 0; i < splitquery.length; i++) {
+                String potentialNum = splitquery[i];
+                if (potentialNum.contains(",")) {
+                    potentialNum = potentialNum.substring(0, potentialNum.length() - 1);
+                }
+                numList.add(convertingStringNumbersToIntegers(potentialNum));
+            }
+            List<Integer> sortedlist = new ArrayList<>(numList);
+            Collections.sort(sortedlist);
+            return sortedlist.get(sortedlist.size() - 1).toString();
+        }
         return "";
     }
+
+    public int convertingStringNumbersToIntegers(String stringNum) {
+        try {
+            int number = Integer.parseInt(stringNum);
+            return number;
+        } catch (NumberFormatException nfe){
+            return 0;
+        }
+      }
 }
